7c373966b104c8fd67963181979bba97fe2bc960
dieptranngocminh <67629587+dieptranngocminh@users.noreply.github.com>
2021-12-31 16:16:43 +0700

    Initial commit

---
+++ app/src/main/java/com/example/logicdesign_project_final/Register.java
@@ -0,0 +1,14 @@
+package com.example.logicdesign_project_final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+}
