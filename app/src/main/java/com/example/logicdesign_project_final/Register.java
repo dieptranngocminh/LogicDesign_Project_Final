dc3160fb24a3019485fccb110617417dd8fb8489
dieptranngocminh <67629587+dieptranngocminh@users.noreply.github.com>
2021-12-31 20:13:28 +0700

    sương sương homepage

--- app/src/main/java/com/example/logicdesign_project_final/Register.java
+++ app/src/main/java/com/example/logicdesign_project_final/Register.java
@@ -3,9 +3,10 @@ package com.example.logicdesign_project_final;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class Register extends AppCompatActivity {
-
+    EditText mFullName, mEmail, mPassWord, mPhone;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
