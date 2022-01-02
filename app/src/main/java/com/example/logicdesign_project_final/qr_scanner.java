05759a519a539e145ca4fb5d2d91343bde57e5e7
dieptranngocminh <67629587+dieptranngocminh@users.noreply.github.com>
2022-01-02 15:12:33 +0700

    user manual và qr scanner

---
+++ app/src/main/java/com/example/logicdesign_project_final/qr_scanner.java
@@ -0,0 +1,14 @@
+package com.example.logicdesign_project_final;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class qr_scanner extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_qr_scanner);
+    }
+}
