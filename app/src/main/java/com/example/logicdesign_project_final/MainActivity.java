53e5235cc8c54ca208038d6dc77489d74fd0c0ab
kinggiaan <56713472+kinggiaan@users.noreply.github.com>
2022-01-04 19:48:27 +0700

    CHINH sua Permission

--- app/src/main/java/com/example/logicdesign_project_final/MainActivity.java
+++ app/src/main/java/com/example/logicdesign_project_final/MainActivity.java
@@ -1,42 +1,66 @@
 package com.example.logicdesign_project_final;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
 import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-    @Override
-    public void onRequestPermissionsResult(int requestCode,
-                                           String permissions[], int[] grantResults) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-        switch (requestCode) {
-            case 1: {
+        //ASK PERMISSION
+        if (ActivityCompat.checkSelfPermission(MainActivity.this
+                , Manifest.permission.INTERNET) == PackageManager.PERMISSION_GRANTED) {
+            //IF GRANTED -> GETLOCATION
 
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0
-                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+        } else {
+            ActivityCompat.requestPermissions(MainActivity.this
+                    , new String[]{Manifest.permission.INTERNET}, 44);
+        }
 
-                    // permission was granted, yay! Do the
-                    // contacts-related task you need to do.
-                } else {
+        if (ActivityCompat.checkSelfPermission(MainActivity.this
+                , Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+            //IF GRANTED -> GETLOCATION
 
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-                    Toast.makeText(MainActivity.this, "Permission denied to read your External storage", Toast.LENGTH_SHORT).show();
-                }
-                return;
-            }
-
-            // other 'case' lines to check for other
-            // permissions this app might request
+        } else {
+            ActivityCompat.requestPermissions(MainActivity.this
+                    , new String[]{Manifest.permission.CAMERA}, 44);
         }
     }
+
+//    ///////////CHECK PERMISSION
+//    private boolean checkIfAlreadyhavePermission() {
+//        int result = ContextCompat.checkSelfPermission(this, Manifest.permission.GET_ACCOUNTS);
+//        if (result == PackageManager.PERMISSION_GRANTED) {
+//            return true;
+//        } else {
+//            return false;
+//        }
+//    }
+//    private void requestForSpecificPermission() {
+//        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.INTERNET,Manifest.permission.CAMERA}, 101);
+//    }
+//    @Override
+//    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+//        switch (requestCode) {
+//            case 101:
+//                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+//                    //granted
+//                } else {
+//                    //not granted
+//                }
+//                break;
+//            default:
+//                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+//        }
+//    }
 }
