248e378ded27a32dad21207cabadf8cf46b2349f
dieptranngocminh <67629587+dieptranngocminh@users.noreply.github.com>
2022-01-04 19:51:09 +0700

    sửa backgr4

--- app/src/main/java/com/example/logicdesign_project_final/personal_inf.java
+++ app/src/main/java/com/example/logicdesign_project_final/personal_inf.java
@@ -1,10 +1,12 @@
 package com.example.logicdesign_project_final;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -20,6 +22,15 @@ public class personal_inf extends AppCompatActivity {
         setContentView(R.layout.activity_personal_inf);
 
         user = FirebaseAuth.getInstance().getCurrentUser();
-        //reference = FirebaseDatabase
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        userID = user.getUid();
+
+        final TextView FullnameTextview = (TextView)findViewById(R.id.Fullname);
+        final TextView DobTextview = (TextView)findViewById(R.id.Dob);
+        final TextView StudentIDTextview = (TextView)findViewById(R.id.StudentID);
+        final TextView EmailTextview = (TextView)findViewById(R.id.Email);
+        final TextView PhonenameTextview = (TextView)findViewById(R.id.Phonenumber);
+        final TextView IDTextview = (TextView)findViewById(R.id.ID);
+        final TextView PasswordTextview = (TextView)findViewById(R.id.Password);
     }
 }
