7f0bad7373390401ebbc03a218813430ea49c95a
dieptranngocminh <67629587+dieptranngocminh@users.noreply.github.com>
2022-01-04 20:59:14 +0700

    User.java

---
+++ app/src/main/java/com/example/logicdesign_project_final/User.java
@@ -0,0 +1,18 @@
+package com.example.logicdesign_project_final;
+
+public class User {
+    public String Fullname, Dob, StudentID, Email, Phonenumber, ID;
+
+    public User(){
+
+    }
+
+    public User(String Fullname, String Dob, String StudentID, String Email, String Phonenumber, String ID){
+        this.Fullname = Fullname;
+        this.Dob = Dob;
+        this.StudentID = StudentID;
+        this.Email = Email;
+        this.Phonenumber = Phonenumber;
+        this.ID = ID;
+    }
+}
