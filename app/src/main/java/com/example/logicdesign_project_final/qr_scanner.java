494b68bfe57826f9a1249bdb3bd8cd0ea28d00a2
kinggiaan <56713472+kinggiaan@users.noreply.github.com>
2022-01-06 15:51:04 +0700

    Done lam QR Scan

--- app/src/main/java/com/example/logicdesign_project_final/qr_scanner.java
+++ app/src/main/java/com/example/logicdesign_project_final/qr_scanner.java
@@ -11,6 +11,10 @@ import android.widget.Toast;
 import com.budiyev.android.codescanner.CodeScanner;
 import com.budiyev.android.codescanner.CodeScannerView;
 import com.budiyev.android.codescanner.DecodeCallback;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.zxing.Result;
 
 
@@ -18,7 +22,9 @@ public class qr_scanner extends AppCompatActivity {
     TextView txt;
     CodeScanner codeScanner;
     CodeScannerView codeScannerView;
-
+    FirebaseAuth mAuth;
+    FirebaseDatabase  rootNode;
+    DatabaseReference reference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,13 +34,25 @@ public class qr_scanner extends AppCompatActivity {
         codeScannerView = (CodeScannerView) findViewById(R.id.scanner_view);
         codeScanner = new CodeScanner(this, codeScannerView);
 
+        // Firebase
+        mAuth = FirebaseAuth.getInstance();
+        reference = FirebaseDatabase.getInstance().getReference("Users");
+        FirebaseUser user = mAuth.getCurrentUser();
+
         codeScanner.setDecodeCallback(new DecodeCallback() {
             @Override
             public void onDecoded(@NonNull Result result) {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        txt.setText(result.getText());
+                        String code = result.getText();
+                        if(checkFormat(code))  {txt.setText(code);}
+                        else{
+                            txt.setText("WRONG FORMAT");
+                        }
+
+
+
                     }
                 });
             }
@@ -51,4 +69,18 @@ public class qr_scanner extends AppCompatActivity {
     private void requestCamera() {
         codeScanner.startPreview();
     }
+
+    private boolean checkFormat(String code){
+        if (code.indexOf("!") == 0 && code.lastIndexOf("#") == code.charAt(code.length()-1) ){
+            return true;
+        }
+        return false;
+    }
+    private String ProcessCode(String code){
+        ///Slpit data
+        code.replace("!","");
+        code.replace("#","");
+    return code;
+    }
+
 }
