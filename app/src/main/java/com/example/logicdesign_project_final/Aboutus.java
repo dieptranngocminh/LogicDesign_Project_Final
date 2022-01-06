a852856a70a41746e82904c28531d01809dc7dbc
dieptranngocminh <67629587+dieptranngocminh@users.noreply.github.com>
2022-01-06 15:49:00 +0700

    add link java

--- app/src/main/java/com/example/logicdesign_project_final/Aboutus.java
+++ app/src/main/java/com/example/logicdesign_project_final/Aboutus.java
@@ -1,4 +1,37 @@
 package com.example.logicdesign_project_final;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class Aboutus {
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+
+public class Aboutus extends AppCompatActivity{
+    TextView link;
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.aboutus);
+
+        link = findViewById(R.id.text22);
+
+        link.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                gotoUrl("https://forms.gle/E8o7CcLqQmwJm5rG6");
+            }
+        });
+    }
+
+    private void gotoUrl(String s) {
+        Uri uri = Uri.parse(s);
+        startActivity(new Intent(Intent.ACTION_VIEW,uri));
+    }
 }
