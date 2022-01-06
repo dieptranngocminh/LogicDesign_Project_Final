eea00dceaae60b67a9a75abd72abeaba3ac7ee8f
dieptranngocminh <67629587+dieptranngocminh@users.noreply.github.com>
2022-01-06 16:05:17 +0700

    sửa about us java

--- app/src/main/java/com/example/logicdesign_project_final/Aboutus.java
+++ app/src/main/java/com/example/logicdesign_project_final/Aboutus.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.text.method.LinkMovementMethod;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -14,24 +15,13 @@ import com.google.firebase.auth.FirebaseAuth;
 
 
 public class Aboutus extends AppCompatActivity{
-    TextView link;
+
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.aboutus);
 
-        link = findViewById(R.id.text22);
-
-        link.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                gotoUrl("https://forms.gle/E8o7CcLqQmwJm5rG6");
-            }
-        });
-    }
-
-    private void gotoUrl(String s) {
-        Uri uri = Uri.parse(s);
-        startActivity(new Intent(Intent.ACTION_VIEW,uri));
+        TextView link = (TextView) findViewById(R.id.text22);
+        link.setMovementMethod(LinkMovementMethod.getInstance());
     }
 }
