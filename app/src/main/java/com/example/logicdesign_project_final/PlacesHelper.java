d7ddb56d80d9ffda3706900a8bd91aa3048da917
kinggiaan <56713472+kinggiaan@users.noreply.github.com>
2022-01-06 22:46:52 +0700

    Dang lam QR Scan dua location

---
+++ app/src/main/java/com/example/logicdesign_project_final/PlacesHelper.java
@@ -0,0 +1,14 @@
+package com.example.logicdesign_project_final;
+
+public class PlacesHelper {
+    String building,room,time;
+
+    public PlacesHelper(){
+    }
+
+    public PlacesHelper(String building,String room,String time){
+        this.building = building;
+        this.room=room;
+        this.time=time;
+    }
+}
--- app/src/main/java/com/example/logicdesign_project_final/qr_scanner.java
+++ app/src/main/java/com/example/logicdesign_project_final/qr_scanner.java
@@ -3,8 +3,10 @@ package com.example.logicdesign_project_final;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -13,18 +15,30 @@ import com.budiyev.android.codescanner.CodeScannerView;
 import com.budiyev.android.codescanner.DecodeCallback;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.zxing.Result;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 
 
 public class qr_scanner extends AppCompatActivity {
     TextView txt;
     CodeScanner codeScanner;
     CodeScannerView codeScannerView;
+
     FirebaseAuth mAuth;
     FirebaseDatabase  rootNode;
     DatabaseReference reference;
+    FirebaseUser user;
+    String userID;
+
+    Button btn_rescan,btn_confirm;
+    //Code
+    String code;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,10 +48,14 @@ public class qr_scanner extends AppCompatActivity {
         codeScannerView = (CodeScannerView) findViewById(R.id.scanner_view);
         codeScanner = new CodeScanner(this, codeScannerView);
 
+        //Button
+        btn_rescan = findViewById(R.id.btn_rescan);
+        btn_confirm =findViewById(R.id.btn_confirm);
+
         // Firebase
         mAuth = FirebaseAuth.getInstance();
-        reference = FirebaseDatabase.getInstance().getReference("Users");
-        FirebaseUser user = mAuth.getCurrentUser();
+
+
 
         codeScanner.setDecodeCallback(new DecodeCallback() {
             @Override
@@ -45,11 +63,12 @@ public class qr_scanner extends AppCompatActivity {
                 runOnUiThread(new Runnable() {
                     @Override
                     public void run() {
-                        String code = result.getText();
+                        code = result.getText();
                         if(checkFormat(code))  {txt.setText(code);}
                         else{
                             txt.setText("WRONG FORMAT");
                         }
+
                         //txt.setText(result.getText());
 
 
@@ -58,6 +77,46 @@ public class qr_scanner extends AppCompatActivity {
             }
         });
 
+        btn_rescan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                codeScanner.setDecodeCallback(new DecodeCallback() {
+                    @Override
+                    public void onDecoded(@NonNull Result result) {
+                        runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                code = result.getText();
+                                if(checkFormat(code))  {txt.setText(code);}
+                                else{
+                                    txt.setText("WRONG FORMAT");
+                                }
+
+                                //txt.setText(result.getText());
+
+
+                            }
+                        });
+                    }
+                });
+            }
+        });
+
+        btn_confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //Take time on phone
+                DateFormat df = new SimpleDateFormat("h:mm a");
+                String date = df.format(Calendar.getInstance().getTime());
+
+                //Fire base
+                user = FirebaseAuth.getInstance().getCurrentUser();
+                reference = FirebaseDatabase.getInstance().getReference("Users");
+                userID = user.getUid();
+
+//                reference.child("places").limitToLast(1)
+            }
+        });
     }
 
     @Override
@@ -83,5 +142,14 @@ public class qr_scanner extends AppCompatActivity {
         code.replace("#","");
     return code;
     }
+//    private boolean CheckBuilding(String building){
+//        rootNode = FirebaseDatabase.getInstance();
+//        reference = rootNode.getReference("Location");
+//
+//        reference.
+//        DataSnapshot snapshot;
+//
+//        return snapshot.hasChild(building);
+//    }
 
 }
