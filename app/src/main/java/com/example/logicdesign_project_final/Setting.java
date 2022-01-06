424cb09af98f3059ef5e708ba7307c1c4a42c5cb
dieptranngocminh <67629587+dieptranngocminh@users.noreply.github.com>
2022-01-06 20:06:14 +0700

    xong layout + java setting ròi nè

--- app/src/main/java/com/example/logicdesign_project_final/Setting.java
+++ app/src/main/java/com/example/logicdesign_project_final/Setting.java
@@ -2,7 +2,10 @@ package com.example.logicdesign_project_final;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Setting extends AppCompatActivity {
 
@@ -10,5 +13,24 @@ public class Setting extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        Button changeprfbtn = (Button)findViewById(R.id.changeprofile);
+        Button deleteaccbtn = (Button)findViewById(R.id.deleteaccount);
+
+        changeprfbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent int1=new Intent(Setting.this, Changeprofile.class);
+                startActivity(int1);
+            }
+        });
+
+        deleteaccbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent int2=new Intent(Setting.this, Deleteaccount.class);
+                startActivity(int2);
+            }
+        });
     }
 }
