410112810415b1d66393012a0185a4fa9b951566
kinggiaan <56713472+kinggiaan@users.noreply.github.com>
2022-01-07 13:47:40 +0700

    Changing in qr scanner

---
+++ app/src/main/java/com/example/logicdesign_project_final/LocationHelper.java
@@ -0,0 +1,13 @@
+package com.example.logicdesign_project_final;
+
+public class LocationHelper {
+    int current, max;
+
+    public LocationHelper() {
+    }
+
+    public LocationHelper(int current, int max) {
+        this.current = current;
+        this.max = max;
+    }
+}
