347e7217d6b8e613a2642beda68fac15f44ba7fa
kinggiaan <56713472+kinggiaan@users.noreply.github.com>
2022-01-07 20:04:38 +0700

    Chinh UI

--- app/src/main/java/com/example/logicdesign_project_final/Changeprofile.java
+++ app/src/main/java/com/example/logicdesign_project_final/Changeprofile.java
@@ -61,13 +61,13 @@ public class Changeprofile extends AppCompatActivity {
         password.setText(PASSWORD);
     }
 
-    public void update(View view){
-        if(isNameChanged() || isDobChanged())
-    }
-
-    private boolean isDobChanged() {
-    }
-
-    private boolean isNameChanged() {
-    }
+//    public void update(View view){
+//        if(isNameChanged() || isDobChanged());
+//    }
+//
+//    private boolean isDobChanged() {
+//    }
+//
+//    private boolean isNameChanged() {
+//    }
 }
